36f1dc244b56035bc52d2170b4b380211cf30586
marius <wangone2008@gmail.com>
2023-09-04 22:11:03 +0800

    Modify tx

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/transaction.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/transaction.ex
@@ -483,11 +483,12 @@ defmodule EthereumJSONRPC.Transaction do
     end
   end
 
+  defp entry_to_elixir(_) do
+    {nil, nil}
+  end
+
   defp entry_to_elixir({key, _}) when key in ~w(l1BatchNumber l1BatchTxIndex accessList chainId) do
     :ignore
   end
 
-  defp entry_to_elixir(_) do
-    {nil, nil}
-  end
 end
