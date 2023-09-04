4165a1ee538ba22ddb5d6697609940e0d59d26bc
marius <wangone2008@gmail.com>
2023-09-04 17:18:38 +0800

    Fix receipt

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/receipt.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/receipt.ex
@@ -319,11 +319,11 @@ defmodule EthereumJSONRPC.Receipt do
     :ignore
   end
 
-  defp entry_to_elixir({key, value}) do
-    {:error, {:unknown_key, %{key: key, value: value}}}
-  end
-
   defp entry_to_elixir({key, _}) when key in ~w(returnData returnCode feeStats l1BlockNumber l1BatchNumber l1BatchTxIndex l2ToL1Logs) do
     :ignore
+  end
+
+  defp entry_to_elixir({key, value}) do
+    {:error, {:unknown_key, %{key: key, value: value}}}
   end
 end
