54a3ca6e4ff2a6c237e510d60efc37902cdc4de9
marius <wangone2008@gmail.com>
2023-09-05 20:05:59 +0800

    Add elixir_to_params

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/transaction.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/transaction.ex
@@ -403,6 +403,55 @@ defmodule EthereumJSONRPC.Transaction do
     end
   end
 
+  def elixir_to_params(
+        %{
+          "blockHash" => block_hash,
+          "blockNumber" => block_number,
+          "chainId" => _, 
+          "from" => from_address_hash,
+          "gas" => gas,
+          "gasPrice" => gas_price,
+          "hash" => hash,
+          "input" => input,
+          "maxFeePerGas" => _, 
+          "maxPriorityFeePerGas" => _,
+          "nonce" => nonce,
+          "r" => r,
+          "s" => s,
+          "to" => to_address_hash,
+          "transactionIndex" => index,
+          "v" => v,
+          "value" => value
+        } = transaction
+      ) do
+    result = %{
+      block_hash: block_hash,
+      block_number: block_number,
+      l1_batch_number: 0,
+      l1_batch_tx_index: 0,
+      from_address_hash: from_address_hash,
+      gas: gas,
+      gas_price: gas_price,
+      hash: hash,
+      index: index,
+      input: input,
+      nonce: nonce,
+      r: r,
+      s: s,
+      to_address_hash: to_address_hash,
+      v: v,
+      value: value,
+      "type" => _, 
+      transaction_index: index
+    }
+
+    if transaction["creates"] do
+      Map.put(result, :created_contract_address_hash, transaction["creates"])
+    else
+      result
+    end
+  end
+
   @doc """
   Extracts `t:EthereumJSONRPC.hash/0` from transaction `params`
 
